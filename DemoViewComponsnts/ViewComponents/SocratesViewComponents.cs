88b19ed92ac592bc19deefbed153c03342f891e8
Renan Osorio <renanosoriogd@gmail.com>
2023-03-31 15:53:58 -0300

    first commit

---
+++ DemoViewComponsnts/ViewComponents/SocratesViewComponents.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using System.Threading.Tasks;
+
+namespace DemoViewComponsnts.ViewComponents
+{
+    [ViewComponent(Name = "ApresentacaoSocrates")]
+    public class ApresentacaoSocratesViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+
+    [ViewComponent(Name = "DetalhesSocrates")]
+    public class DetalhesSocratesViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
